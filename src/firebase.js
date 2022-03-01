3c66371677ac6b0147ab679357d5483b1c354978
michael rodrigues <michaelbrunorodrigues@gmail.com>
2022-03-01 17:09:33 +1300

    ready to deploy

--- src/firebase.js
+++ src/firebase.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+  measurementId: "G-JTNZSG972R"
 };
 
 // Initialize Firebase
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -73,6 +73,7 @@ const Login = () => {
         </div>
       </form>
     </section>
+    
   );
 };
 
